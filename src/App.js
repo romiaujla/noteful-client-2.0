4e23a3fd26e42461f3b332f4d925b074ae7ac26d
Raman Aujla <ramanaujla@Ramneets-MacBook-Air.local>
2019-08-14 16:45:34 -0400

    add app button style

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <main className="App">
+      <header>
+        <div className='app-title'>
+          <h1>Noteful</h1>
+        </div>
+        <button className='app-btn'>Button Style</button>
+      </header>
+    </main>
   );
 }
 
