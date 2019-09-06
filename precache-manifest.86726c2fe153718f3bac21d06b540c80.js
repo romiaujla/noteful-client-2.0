ae10928696ec958f75f171ac2152e52e27af8920
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-09-06 19:36:48 -0400

    Updates

---
+++ precache-manifest.86726c2fe153718f3bac21d06b540c80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8edf0355e3a25d73f0c5fa1e595413",
+    "url": "/noteful-client-2.0/index.html"
+  },
+  {
+    "revision": "fb689f8ee21cfeb33d28",
+    "url": "/noteful-client-2.0/static/css/main.1e31c3ac.chunk.css"
+  },
+  {
+    "revision": "42bc5deb8aa734ade109",
+    "url": "/noteful-client-2.0/static/js/2.57ce95ae.chunk.js"
+  },
+  {
+    "revision": "fb689f8ee21cfeb33d28",
+    "url": "/noteful-client-2.0/static/js/main.b1e054d9.chunk.js"
+  },
+  {
+    "revision": "fbfcfc2063e2abf676b4",
+    "url": "/noteful-client-2.0/static/js/runtime~main.c70d1d23.js"
+  }
+]);
